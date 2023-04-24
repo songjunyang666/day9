06f9075063ddeb68582c422affffaf167b793100
song <sjy2241552205@outlook.com>
2023-04-24 20:28:30 +0800

    Initial commit

---
+++ src/Account.java
@@ -0,0 +1,66 @@
+/*
+账户类
+ */
+
+public class Account {
+    private  String cardId;//卡号
+    private String userName;//客户名称
+    private String passWord;//密码
+    private double money;//余额
+    private  double quotaMoney;//当前提现金额
+
+    public Account() {
+    }
+
+    public Account(String cardId, String userName, String passWord,  double quotaMoney) {
+        this.cardId = cardId;
+        this.userName = userName;
+        this.passWord = passWord;
+
+        this.quotaMoney = quotaMoney;
+    }
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public double getQuotaMoney() {
+        return quotaMoney;
+    }
+
+    public void setQuotaMoney(double quotaMoney) {
+        this.quotaMoney = quotaMoney;
+    }
+
+
+
+
+}
